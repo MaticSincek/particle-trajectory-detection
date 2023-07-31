031d682cfb3c3b75b4b7922d9112463191d80519
MaticSincek <matic.sincek97@gmail.com>
2023-07-31 14:40:27 +0200

    Origin at 0,0, custom layers, code quality impro.

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def circle_from_points(p1, p2, p3):
     radius = np.sqrt((cx - p1[0])**2 + (cy - p1[1])**2)
     return ((cx, cy), radius)
 
-def angle_of_point_relative_to_origin(x_origin, y_origin, x, y):
-    angle_rad = math.atan2(y - y_origin, x - x_origin)
+def angle_of_point_relative_to_origin(x, y):
+    angle_rad = math.atan2(y, x)
     angle_rad = (angle_rad + 2 * math.pi) % (2 * math.pi)
     return angle_rad
 
@@ -58,39 +58,45 @@ def get_orientation(p1, p2, p3):
 		# collinear orientation
 		return 0
     
-def cartesian2polar(orig, p):
+def cartesian2polar(p):
     x,y = p
-    angle = angle_of_point_relative_to_origin(orig[0], orig[1], x, y)
-    distance = math.sqrt((orig[0] - x) ** 2 + (orig[1] - y) ** 2)
+    angle = angle_of_point_relative_to_origin(x, y)
+    distance = math.sqrt(x ** 2 + y ** 2)
     return (distance, angle)
 
-def polar2cartesian(orig, distance, angle):
-    x = distance * math.cos(angle) + orig[0]
-    y = distance * math.sin(angle) + orig[1]
+def polar2cartesian(distance, angle):
+    x = distance * math.cos(angle)
+    y = distance * math.sin(angle)
     return (x,y)
 
-def centralize_point_on_sensor(orig, p, sensor_segment_angle, layer_list, layer):
-    distance, angle = cartesian2polar(orig, p)
+def centralize_point_on_sensor(p, sensor_segment_angle, layer_list, layer):
+    distance, angle = cartesian2polar(p)
     new_angle = (int(angle / sensor_segment_angle) * sensor_segment_angle) + sensor_segment_angle / 2
     new_distance = layer_list[layer]
-    return polar2cartesian(orig, new_distance, new_angle)
+    return polar2cartesian(new_distance, new_angle)
 
-def random_point_on_sensor(orig, p, sensor_segment_angle):
-    distance, angle = cartesian2polar(orig, p)
+def random_point_on_sensor(p, sensor_segment_angle):
+    distance, angle = cartesian2polar(p)
     a_min = int(angle / sensor_segment_angle) * sensor_segment_angle
     a_max = (int(angle / sensor_segment_angle) + 1) * sensor_segment_angle
     a_delta = a_max - a_min
     a = a_min + random.random() * a_delta
-    return polar2cartesian(orig, distance, a)
+    return polar2cartesian(distance, a)
+
+def scale_for_drawing (x):
+    return W/2 / 10000 * x
+
 
 W = 1500
 H = 1500
-N_CONCENTRIC = 20
-N_TRAJECTORIES = 3
+realW = 20000
+realH = 20000
+N_CONCENTRIC = 23
+N_TRAJECTORIES = 30
 SENSOR_DENSITY = 3600
-N_SEED_CORRECTIONS = 30*30
-TOLERANCE = 10
-CENTER_TOLERANCE = 2
+N_SEED_CORRECTIONS = 30 ** 3
+TOLERANCE = 50 ** 2
+CENTER_TOLERANCE = 10
 TRAJECTORY_ANGLE_TOLERANCE = 0.87266 # 50deg
 SEED_ANGLE_TOLERANCE = 0.17453 # 10deg
 MIN_PERC_COVERAGE_FOR_TRAJ = 0.9
@@ -112,22 +118,19 @@ detections = []
 origin = (W/2, H/2)
 
 # minimum and maximum radius of the trajectories
-rmin = W * 2 / 3 / 2
-rmax = W * 2 / 3
+rmin = realW * 2 / 3 / 2
+rmax = realW * 2 / 3
 
 img = Image.new("RGB", (W, H))
 canvas = ImageDraw.Draw(img)
 
 draw_point(canvas, origin, 6, "white")
 
-#calculate the number of concentric circles by dividing half of the screen by N_CONCENTRIC
-concentric_space = int(W / 2 - 15)
-concentric_step = int(concentric_space / N_CONCENTRIC)
-layer_radii = [r for r in range(concentric_step, concentric_space + 1, concentric_step)]
+layer_radii = [400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000]
 
 # detector layer drawing and sensor generation
 for r in layer_radii:
-    draw_concentric(canvas, origin, r, 3)
+    draw_concentric(canvas, origin, scale_for_drawing(r), 3)
 
 # generation of arcs
 for i in range(N_TRAJECTORIES):
@@ -145,16 +148,20 @@ for i in range(N_TRAJECTORIES):
         aend = angledeg
 
     # points r away from the origin with an angle "angle" relative to the origin 
-    x = origin[0] + int(r * math.cos(angle))
-    y = origin[1] + int(r * math.sin(angle))
-
-    # center of bbox is in (x,y) with corners r away from the center in both directions
-    bbox = [(x - r, y - r), (x + r, y + r)]
+    x = r * math.cos(angle)
+    y = r * math.sin(angle)
 
     radii.append(r)
     angles.append(angle)
     directions.append(dir)
     centers.append((x,y))
+
+    x_draw = scale_for_drawing(x)
+    y_draw = scale_for_drawing(y)
+    r_draw = scale_for_drawing(r)
+
+    # center of bbox is in (x,y) with corners r away from the center in both directions
+    bbox = [(x_draw - r_draw + origin[0], y_draw - r_draw + origin[1]), (x_draw + r_draw + origin[0], y_draw + r_draw + origin[1])]
 
     canvas.arc(
         bbox, 
@@ -169,24 +176,26 @@ for i in range(N_TRAJECTORIES):
 
     # we already have the trajectory now let's see where it intersects each concentric circle
     detection = []
-    for d2 in layer_radii:
-        d = math.sqrt((x - origin[0]) ** 2 + (y - origin[1]) ** 2)
-        a = (d2 ** 2 - r ** 2 + d ** 2) / (2 * d)
-        h = math.sqrt(d2 ** 2 - a ** 2)
-        x2 = origin[0] + a * (x - origin[0]) / d   
-        y2 = origin[1] + a * (y - origin[1]) / d
+    for radius in layer_radii:
+        d = math.sqrt(x ** 2 + y ** 2)
+        a = (radius ** 2 - r ** 2 + d ** 2) / (2 * d)
+        h = math.sqrt(radius ** 2 - a ** 2)
+        x2 = a * x / d   
+        y2 = a * y / d
 
         if dir == 0:  
-            x3 = x2 + h * (y - origin[1]) / d
-            y3 = y2 - h * (x - origin[0]) / d
+            x3 = x2 + h * y / d
+            y3 = y2 - h * x / d
         else:
-            x3 = x2 - h * (y - origin[1]) / d
-            y3 = y2 + h * (x - origin[0]) / d
+            x3 = x2 - h * y / d
+            y3 = y2 + h * x / d
 
         detection.append((x3,y3))
 
         if not WITH_SENSORS:
-            draw_point(canvas, (x3,y3), 7, "red")
+            x3_real = scale_for_drawing(x3) + origin[0]
+            y3_real = scale_for_drawing(y3) + origin[1]
+            draw_point(canvas, (x3_real,y3_real), 7, "red")
 
     detections.append(detection)
 
@@ -204,7 +213,7 @@ segment_angle = 2 * math.pi / SENSOR_DENSITY
 if WITH_SENSORS:
     for i in range(len(detections_on_layer)):
         for j in range(len(detections_on_layer[i])):
-            p = centralize_point_on_sensor(origin, detections_on_layer[i][j], segment_angle, layer_radii, i)
+            p = centralize_point_on_sensor(detections_on_layer[i][j], segment_angle, layer_radii, i)
             detections_on_layer[i][j] = (float(p[0]), float(p[1]))
 
 # remove points that translated to the same sensor to avoid duplicate trajectories
@@ -214,6 +223,7 @@ for i in range(len(detections_on_layer)):
 marked_for_deletion = []
 
 #delete some points to simulate the real environment whene not every point on trajectory is detected
+#important that we start from the back of the rows for deleting - otherwise we can get indexes that can't be deleted
 if WITH_SENSORS:
     for i in range(len(detections_on_layer)-1, -1, -1):
         for j in range(len(detections_on_layer[i])-1, -1, -1):
@@ -221,7 +231,9 @@ if WITH_SENSORS:
             if chance <= DETECTION_FAIL_RATE:
                 marked_for_deletion.append((i,j))
             else:
-                draw_point(canvas, detections_on_layer[i][j], 6, "yellow")
+                x_draw = scale_for_drawing(detections_on_layer[i][j][0]) + origin[0]
+                y_draw = scale_for_drawing(detections_on_layer[i][j][1]) + origin[1]
+                draw_point(canvas, (x_draw, y_draw), 6, "yellow")
                 
 for (i,j) in marked_for_deletion:
     del detections_on_layer[i][j]
@@ -253,9 +265,9 @@ for p0 in detections_on_layer[N_CONCENTRIC-1]:
 
             min_avg_error = sys.float_info.max
 
-            angle_p0 = angle_reference = angle_of_point_relative_to_origin(origin[0], origin[1], p0[0], p0[1])
-            angle_p1 = angle_of_point_relative_to_origin(origin[0], origin[1], p1[0], p1[1])
-            angle_p2 = angle_of_point_relative_to_origin(origin[0], origin[1], p2[0], p2[1])
+            angle_p0 = angle_reference = angle_of_point_relative_to_origin(p0[0], p0[1])
+            angle_p1 = angle_of_point_relative_to_origin(p1[0], p1[1])
+            angle_p2 = angle_of_point_relative_to_origin(p2[0], p2[1])
 
             # if both points are roughly in the same direction from the origin we continue
             if (abs(angle_p0 - angle_p1) < SEED_ANGLE_TOLERANCE or \
@@ -265,9 +277,9 @@ for p0 in detections_on_layer[N_CONCENTRIC-1]:
                 
                 # we generate random points in sensos space and figure out which one fits best
                 for _ in range(N_SEED_CORRECTIONS):
-                    pp0 = random_point_on_sensor(origin, p0, segment_angle)
-                    pp1 = random_point_on_sensor(origin, p1, segment_angle)
-                    pp2 = random_point_on_sensor(origin, p2, segment_angle)
+                    pp0 = random_point_on_sensor(p0, segment_angle)
+                    pp1 = random_point_on_sensor(p1, segment_angle)
+                    pp2 = random_point_on_sensor(p2, segment_angle)
 
                     # find the r and center of these 3 points
                     (center, r) = circle_from_points(pp0, pp1, pp2)
@@ -276,23 +288,21 @@ for p0 in detections_on_layer[N_CONCENTRIC-1]:
                         continue
 
                     # check if point goes roughly through the center
-                    distance_center_origin = math.sqrt((center[0] - origin[0]) ** 2 + (center[1] - origin[1]) ** 2)
+                    distance_center_origin = math.sqrt(center[0] ** 2 + center[1] ** 2)
                     center_error = abs(distance_center_origin-r)
                     
                     if center_error > CENTER_TOLERANCE:
                         continue
                     
                     # variables that hold how many points we found and cumulative distance of points to sensor centers
-                    points_on_seed_trajectory = 3
-                    cumul_error = (math.sqrt((pp0[0] - p0[0]) ** 2 + (pp0[1] - p0[1]) ** 2)) ** 2 + \
-                                    (math.sqrt((pp1[0] - p1[0]) ** 2 + (pp1[1] - p1[1]) ** 2)) ** 2 + \
-                                    (math.sqrt((pp2[0] - p2[0]) ** 2 + (pp2[1] - p2[1]) ** 2)) ** 2 
+                    points_on_seed_trajectory = 0
+                    cumul_error = 0
                     
                     # if r makes sense it could be a trajectory
                     if r < rmax and r > rmin:
 
                         # check how the seed is supported by points by goung through all the layers and finding the best fit on every layer
-                        for layer in range(N_CONCENTRIC-4, -1, -1):
+                        for layer in range(N_CONCENTRIC-1, -1, -1):
                             min_error = sys.float_info.max
 
                             for det in range(len(detections_on_layer[layer])):
@@ -303,17 +313,16 @@ for p0 in detections_on_layer[N_CONCENTRIC-1]:
 
                                 # if distance from the seed center to point is approx. the same as r 
                                 # and the angle from origin is similar it is on the trajectory of seed s
-                                error = abs(distance_center_current_point-r)
+                                error = abs(distance_center_current_point-r) ** 2
                                 if error < TOLERANCE:
-                                    detection_angle = angle_of_point_relative_to_origin(origin[0], origin[1], p[0], p[1])
-                                    sq_err = error * error
+                                    detection_angle = angle_of_point_relative_to_origin(p[0], p[1])
 
                                     if (abs(angle_reference - detection_angle) < TRAJECTORY_ANGLE_TOLERANCE) or \
                                         abs(angle_reference - detection_angle) > (2 * math.pi - TRAJECTORY_ANGLE_TOLERANCE):
 
                                         # if it's the best fit as of now save the error as minimal
-                                        if sq_err < min_error:
-                                            min_error = sq_err
+                                        if error < min_error:
+                                            min_error = error
 
                             # if we ever found a point that is close, then means we found the closest one then add it to the cumulative
                             if (min_error < sys.float_info.max):
@@ -328,15 +337,16 @@ for p0 in detections_on_layer[N_CONCENTRIC-1]:
                             r_best = r
                             pp0_best = pp0
                             pp1_best = pp1
+                            pp2_best = pp2
                             center_best = center
 
             # if we found at least one trajectory we can be sure its the best one and we can save it
             if center_best != None:
-                o = get_orientation(origin,pp1_best,pp0_best) 
+                o = get_orientation(pp2_best, pp1_best, pp0_best) 
                 trajectory_radii.append(r_best)
                 trajectory_centers.append(center_best)
                 trajectory_directions.append(o)
-                trajectory_points.append((origin,pp0_best,pp1_best,pp2_best))
+                trajectory_points.append((pp0_best, pp1_best, pp2_best))
 
 print("Found " + str(len(trajectory_radii)) + " out of " + str(N_TRAJECTORIES) + " trajectories." )
 
@@ -345,7 +355,7 @@ for i in range(len(trajectory_radii)):
     r = trajectory_radii[i]
     dir = trajectory_directions[i]
     center = trajectory_centers[i]
-    angle = angle_of_point_relative_to_origin(origin[0], origin[1], center[0], center[1])
+    angle = angle_of_point_relative_to_origin(center[0], center[1])
 
     # we should always only draw half of the circle
     angledeg = math.degrees(angle)
@@ -356,8 +366,11 @@ for i in range(len(trajectory_radii)):
         astart = (360 + angledeg - 180) % 360
         aend = angledeg
 
+    r_draw = scale_for_drawing(r)
+    center_draw = (scale_for_drawing(center[0]) + origin[0], scale_for_drawing(center[1]) + origin[1])
+
     # calculate bbox from center and r
-    bbox = [(center[0] - r, center[1] - r), (center[0] + r, center[1] + r)]
+    bbox = [(center_draw[0] - r_draw, center_draw[1] - r_draw), (center_draw[0] + r_draw, center_draw[1] + r_draw)]
 
     canvas.arc(
         bbox, 
